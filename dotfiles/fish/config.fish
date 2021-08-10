66353a797c7ef8861a97f8dccc747cd57bcd0714
wuallanx <wuallanx@gmail.com>
2021-08-10 22:11:31 +0700

    Add Comic Mono fonts. Update dot files

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -151,5 +151,5 @@ alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 
 ## Run paleofetch if session is interactive
 # if status --is-interactive
-#   neofetch
+#    neofetch
 # end
